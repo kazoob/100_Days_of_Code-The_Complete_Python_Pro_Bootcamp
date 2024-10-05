63edb33e1b27d068204a9f8c6e6374d75db19239
Kazoob <>
2024-10-05 18:45:04 -0500

    Day 2 solution

--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -3,4 +3,6 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 
+per_person = bill * (1 + (tip / 100)) / people
 
+print(f"Each person should pay: ${per_person:,.2f}")
