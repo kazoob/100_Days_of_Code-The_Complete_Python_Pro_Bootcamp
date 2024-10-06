045b57b08977e8e36d40dd532d73d47c72a96c70
Kazoob <>
2024-10-06 11:15:51 -0500

    Day 4 random add count, do 1000 iterations

--- Day 4/Random Module/task.py
+++ Day 4/Random Module/task.py
@@ -1,9 +1,16 @@
 import random
 
-for x in range(20):
+heads = 0
+tails = 0
+
+for x in range(1000):
     coin = random.randint (1,2)
 
     if coin == 1:
+        heads += 1
         print(f"{x}: Heads")
     else:
+        tails += 1
         print(f"{x}: Tails")
+
+print(f"\nHeads: {heads}, Tails: {tails}")
