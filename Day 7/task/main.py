22fd5e10420e721be8ec889cdd655bce0db0b094
Kazoob <>
2024-10-08 10:11:42 -0500

    Day 7 step 2 solution (no optimizations as file will be overwritten in the next step)

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -4,15 +4,28 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
+# Create a "placeholder" with the same number of blanks as the chosen_word
+
+placeholder = ""
+for letter in chosen_word:
+    placeholder += "_"
+
+print(placeholder)
 
 guess = input("Guess a letter: ").lower()
 
-# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+# Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+
+display = ""
 
 for letter in chosen_word:
     if letter == guess:
+        display += letter
         print("Right")
     else:
+        display += "_"
         print("Wrong")
 
+print(display)
+
+placeholder = display
