ee8b76fea4ac07e34f8606923b29c1bb738cd7bb
Kazoob <>
2024-10-08 14:24:32 -0500

    Day 9 blind auction solution

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,30 @@
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
+from art import logo
 
+prompt_user = True
+bids = {}
 
+while prompt_user:
+    print(logo)
+
+    # Ask the user for input
+    name = input("Please enter your name: ")
+    bid = round(float(input("Please enter your bid: $")),2)
+
+    # Save data into dictionary {name: price}
+    bids[name] = bid
+
+    # Whether if new bids need to be added
+    prompt_user_input = ""
+    while not (prompt_user_input.startswith("y") or prompt_user_input.startswith("n")):
+        prompt_user_input = input("Are there any other bidders (y/n): ").lower()
+
+    if prompt_user_input.startswith("n"):
+        prompt_user = False
+
+# Compare bids in dictionary using sorted()
+#bids_ordered = sorted(bids.items(), key=lambda item: item[1], reverse=True)
+#print(f"The winner is {bids_ordered[0][0]} with a bid of ${bids_ordered[0][1]:,.2f}")
+
+# Compare bids in dictionary using max()
+highest_bidder = max(bids, key=bids.get)
+print(f"The winner is {highest_bidder} with a bid of ${bids[highest_bidder]:,.2f}")
