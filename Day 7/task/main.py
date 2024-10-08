bca5b2caebcf6836f5777ec14f58907c5bd4d61c
Kazoob <>
2024-10-08 10:29:44 -0500

    Day 7 step 4 initial file

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,5 +1,64 @@
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["aardvark", "baboon", "camel"]
+
+# TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
+#  Set 'lives' to equal 6.
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -10,33 +69,31 @@ for position in range(word_length):
     placeholder += "_"
 print(placeholder)
 
-# Use a while loop to let the user guess again.
+game_over = False
+correct_letters = []
 
-gameover = False
-
-while not gameover:
+while not game_over:
     guess = input("Guess a letter: ").lower()
 
     display = ""
 
-    # Change the for loop so that you keep the previous correct letters in display.
-
-    for x in range(0, len(chosen_word)):
-        letter = chosen_word[x]
-
-        if placeholder[x] == "_":
-            if letter == guess:
-                display += letter
-            else:
-                display += "_"
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
         else:
-            display += placeholder[x]
+            display += "_"
 
     print(display)
 
-    placeholder = display
+    # TODO-2: - If guess is not a letter in the chosen_word, Then reduce 'lives' by 1.
+    #  If lives goes down to 0 then the game should stop and it should print "You lose."
 
-    if placeholder == chosen_word:
-        gameover = True
+    if "_" not in display:
+        game_over = True
+        print("You win.")
 
-print("Game over!")
+    # TODO-3: - print the ASCII art from 'stages'
+    #  that corresponds to the current number of 'lives' the user has remaining.
