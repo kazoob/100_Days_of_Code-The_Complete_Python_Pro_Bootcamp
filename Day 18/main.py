3cd66bca945385c5324e4908b0efe79f50482e6c
Kazoob <>
2024-10-12 11:38:56 -0500

    Day 18 challenge 3

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 scr = Screen()
 # rootwindow = scr.getcanvas().winfo_toplevel()
@@ -8,8 +9,6 @@ scr = Screen()
 turt = Turtle()
 turt.shape("turtle")
 turt.color("orange")
-
-turt.teleport(scr.canvwidth * -1, 0)
 
 # Challenge 1
 # turt.forward(100)
@@ -26,6 +25,8 @@ turt.teleport(scr.canvwidth * -1, 0)
 
 # Challenge 2
 
+# turt.teleport(scr.canvwidth * -1, 0)
+
 # for i in range(0,50):
 #     if i % 2 != 0:
 #         turt.penup()
@@ -35,6 +36,17 @@ turt.teleport(scr.canvwidth * -1, 0)
 
 # Challenge 3
 
+for i in range(3,11):
+    max_rgb = int(scr.colormode())
+    r = random.randint(0, max_rgb)
+    g = random.randint(0, max_rgb)
+    b = random.randint(0, max_rgb)
+
+    turt.pencolor(r, g, b)
+
+    for s in range(0,i):
+        turt.forward(100)
+        turt.right(360/i)
 
 
 
