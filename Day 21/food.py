8aa41617c6aaf1af010897f7243ab1c7b225a70a
Kazoob <>
2024-10-13 22:46:37 -0500

    Day 21 snake game add food class

---
+++ Day 21/food.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+    x_min = None
+    x_max = None
+    y_min = None
+    y_max = None
+
+    def __init__(self, screen_width, screen_height):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.color("orange")
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        self.speed("fastest")
+
+        self.x_min = int(((screen_width / 2) - 20) * -1)
+        self.x_max = int((screen_height / 2) - 20)
+        self.y_min = int(((screen_width / 2) - 20) * -1)
+        self.y_max = int((screen_height / 2) - 20)
+
+        self.move()
+
+    def move(self):
+        x_pos = random.randint(self.x_min, self.x_max + 1)
+        y_pos = random.randint(self.y_min, self.y_max + 1)
+
+        self.teleport(x=x_pos, y=y_pos)
--- Day 21/main.py
+++ Day 21/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 SCREEN_WIDTH = 600
@@ -11,11 +12,12 @@ SNAKE_INITIAL_LENGTH = 3
 SNAKE_COLOR = "white"
 
 scr = Screen()
-scr.setup(width=SCREEN_WIDTH, height=SCREEN_WIDTH)
+scr.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 scr.title(SCREEN_TITLE)
 scr.bgcolor(SCREEN_BGCOLOR)
 
 snake = Snake(length=SNAKE_INITIAL_LENGTH, color=SNAKE_COLOR)
+food = Food(screen_width=SCREEN_WIDTH, screen_height=SCREEN_HEIGHT)
 
 scr.listen()
 scr.onkeypress(key="w", fun=snake.set_direction_up)
@@ -26,12 +28,16 @@ scr.onkeypress(key="Up", fun=snake.set_direction_up)
 scr.onkeypress(key="Down", fun=snake.set_direction_down)
 scr.onkeypress(key="Left", fun=snake.set_direction_left)
 scr.onkeypress(key="Right", fun=snake.set_direction_right)
-scr.onkeypress(key="space", fun=snake.start)
+scr.onkeypress(key="space", fun=snake.start_stop)
 scr.tracer(0)
 
 while not snake.is_game_over():
     snake.move_forward()
     scr.update()
+
+    if snake.head.distance(food) < 15:
+        food.move()
+
     time.sleep(0.1)
 
 scr.exitonclick()
