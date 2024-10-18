160106526ac85a849d11790a694b037382fa3018
Kazoob <>
2024-10-17 23:05:59 -0500

    Day 22 pong game - board.py replace multiple line passes with turtle.pensize, optimize -1 multiplication

--- Day 22/board.py
+++ Day 22/board.py
@@ -1,6 +1,8 @@
 from turtle import Turtle
 
 LINE_SPACING = 20
+LINE_THICKNESS = 3
+LINE_COLOR = "white"
 
 
 class Board(Turtle):
@@ -9,30 +11,32 @@ class Board(Turtle):
         """Create a new board with a center line down the middle, provided by screen_height."""
         super().__init__()
 
-        # Set up the turtle
-        self.color("white")
+        # Set up the turtle.
+        self.color(LINE_COLOR)
         self.speed("fastest")
         self.hideturtle()
+        self.pensize(LINE_THICKNESS)
 
         # Turn off screen updates until completed.
         self.screen.tracer(0)
 
         # Draw the center line.
         y_max = int(screen_height / 2)
-        y_min = -1 * int(screen_height / 2)
+        y_min = -y_max
 
-        # Draw a 3 pixel thick line.
-        for x_pos in range(-1, 2):
-            self.teleport(x=x_pos, y=y_min)
-            self.pendown()
+        # Start at the bottom of the screen.
+        self.teleport(x=0, y=y_min)
+        self.pendown()
 
-            # Draw line from y_min to y_max, alternating pendown and penup every LINE_SPACING
-            for y_pos in range(y_min, y_max + 1, LINE_SPACING):
-                self.goto(x=x_pos, y=y_pos)
-                if self.isdown():
-                    self.penup()
-                else:
-                    self.pendown()
+        # Move to top of the screen.
+        for y_pos in range(y_min, y_max + 1, LINE_SPACING):
+            self.goto(x=0, y=y_pos)
+
+            # Draw the dashes.
+            if self.isdown():
+                self.penup()
+            else:
+                self.pendown()
 
         # Update the screen.
         self.screen.tracer(1)
