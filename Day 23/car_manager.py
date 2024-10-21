d2bbb4f0b02576ab040bc3321604a5f1206a29a5
Kazoob <>
2024-10-20 23:41:17 -0500

    Day 23 turtle crossing game - car manager car spawning, movement

--- Day 23/car_manager.py
+++ Day 23/car_manager.py
@@ -1,3 +1,6 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
@@ -5,10 +8,44 @@ MOVE_INCREMENT = 10
 
 class CarManager:
     def __init__(self):
-        pass
+        self.move_distance = STARTING_MOVE_DISTANCE
+        self.cars = []
 
     def move(self):
-        pass
+        for car in self.cars:
+            if car.move_forward(self.move_distance) >= 300:
+                self.cars.remove(car)
+
+        self.new_car()
 
     def level_up(self):
-        pass
+        self.move_distance += MOVE_INCREMENT
+
+    def new_car(self):
+        self.cars.append(Car())
+
+
+class Car:
+    def __init__(self):
+        color = random.choice(COLORS)
+        y_pos = random.randint(-250, 260)
+
+        self.front = self.new_section(color, 300, y_pos)
+        self.back = self.new_section(color, 320, y_pos)
+
+    def new_section(self, color, x_pos, y_pos):
+        section = Turtle()
+        section.shape("square")
+        section.color(color)
+        section.penup()
+        section.setheading(180)
+        section.speed("fastest")
+        section.teleport(x=x_pos, y=y_pos)
+
+        return section
+
+    def move_forward(self, distance):
+        self.front.forward(distance)
+        self.back.forward(distance)
+
+        return self.front.xcor()
