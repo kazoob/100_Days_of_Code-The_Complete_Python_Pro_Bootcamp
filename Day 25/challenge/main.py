1da82ba1640559919503425d97e2459054894467
Kazoob <>
2024-10-21 23:39:48 -0500

    Day 25 challenge - challenge 1 and 2

---
+++ Day 25/challenge/main.py
@@ -0,0 +1,27 @@
+# Challenge 1
+
+# with open("weather_data.csv") as weather_file:
+#     data = weather_file.readlines()
+# print(data)
+
+
+# Challenge 2
+
+# import csv
+#
+# with open("weather_data.csv") as weather_file:
+#     # Skip first line
+#     next(weather_file)
+#     data = csv.reader(weather_file)
+#     temperatures = []
+#
+#     for row in data:
+#         print(row)
+#         temperatures.append(int(row[1]))
+#
+#     print(temperatures)
+
+# Challenge 3
+
+import pandas
+
