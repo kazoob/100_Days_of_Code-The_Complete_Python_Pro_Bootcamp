42f8fba234c2d349f01722081349044da1cfa194
Kazoob <>
2024-10-22 00:01:05 -0500

    Day 25 challenge - challenge 4

--- Day 25/challenge/main.py
+++ Day 25/challenge/main.py
@@ -23,8 +23,42 @@
 
 # Challenge 3
 
+# import pandas
+#
+# data = pandas.read_csv("weather_data.csv")
+#
+# print(data["temp"])
+
+# Challenge 4
+
 import pandas
+from numpy.ma.extras import average
 
 data = pandas.read_csv("weather_data.csv")
-print(data)
-print(data["temp"])
+print(data.to_dict())
+print()
+
+temp_list = data.temp.to_list()
+print(temp_list)
+print()
+
+print(f"Average temperature (numpy): {int(average(temp_list))}")
+print(f"Average temperature (panda): {int(data.temp.mean())}")
+print()
+
+print(f"Maximum temperature (panda): {data.temp.max()}")
+print(f"Minimum temperature (panda): {data.temp.min()}")
+print()
+
+print(data[data.day == "Monday"])
+print(data[data.day == "Monday"].condition)
+print()
+
+print(data[data.temp == data.temp.max()])
+print(data[data.temp == data.temp.max()].temp)
+print()
+
+temp = data[data.day == "Monday"].temp[0]
+print(f"{(temp * 9/5) + 32} F")
+print()
+
