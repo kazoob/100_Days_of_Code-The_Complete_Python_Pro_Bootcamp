b55e8233135ae2473427c7f3f552a4b01e12241f
Kazoob <>
2024-10-26 23:36:35 -0500

    Day 29 password manager - password generator optimize list comprehension

--- Day 29/main.py
+++ Day 29/main.py
@@ -22,15 +22,11 @@ def generate_password():
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    nr_letters = random.randint(8, 10)
-    nr_symbols = random.randint(2, 4)
-    nr_numbers = random.randint(2, 4)
-
     password_list = []
 
-    password_list.extend([random.choice(letters) for _ in range(nr_letters)])
-    password_list.extend([random.choice(symbols) for _ in range(nr_symbols)])
-    password_list.extend([random.choice(numbers) for _ in range(nr_numbers)])
+    password_list.extend([random.choice(letters) for _ in range(random.randint(8, 10))])
+    password_list.extend([random.choice(symbols) for _ in range(random.randint(2, 4))])
+    password_list.extend([random.choice(numbers) for _ in range(random.randint(2, 4))])
 
     random.shuffle(password_list)
 
