2451e469af80d2abb2197ffb264a51a94d564ec1
Kazoob <>
2024-10-26 23:27:26 -0500

    Day 29 password manager - generate password function

--- Day 29/main.py
+++ Day 29/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 WINDOW_PADDING_X = 50
 WINDOW_PADDING_Y = 50
@@ -14,8 +15,36 @@ PASSWORD_SEPARATOR = "|"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    """Generate a random password. Copy to the clipboard."""
-    pass
+    """Generate a random password. Copy to the clipboard. Code from Day 5."""
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    for char in range(nr_letters):
+        password_list.append(random.choice(letters))
+
+    for char in range(nr_symbols):
+        password_list += random.choice(symbols)
+
+    for char in range(nr_numbers):
+        password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+
+    password_input.delete(0, END)
+    password_input.insert(END, password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
