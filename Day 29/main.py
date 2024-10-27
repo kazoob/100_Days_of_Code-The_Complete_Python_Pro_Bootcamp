2fb73e52f1821f8bbfd22ca64e3106c1d7a09297
Kazoob <>
2024-10-26 19:37:20 -0500

    Day 29 password manager - ui elements

--- Day 29/main.py
+++ Day 29/main.py
@@ -7,8 +7,11 @@ LOGO_IMAGE = "logo.png"
 LOGO_PADDING = 1
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
+def generate_password():
+    pass
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    pass
 
 # ---------------------------- UI SETUP ------------------------------- #
 # Create the window
@@ -24,9 +27,43 @@ logo_img_width = logo_img.width()
 logo_img_height = logo_img.height()
 
 # Create the logo canvas
-logo_canvas = Canvas(width=logo_img_width + LOGO_PADDING * 2, height=logo_img_height + LOGO_PADDING * 2, highlightthickness=0)
-logo_canvas.create_image(logo_img_width / 2 + LOGO_PADDING, logo_img_width / 2 + LOGO_PADDING, image=logo_img)
-logo_canvas.grid(column=1, row=1)
+logo_canvas_width = logo_img_width + LOGO_PADDING * 2
+logo_canvas_height = logo_img_height + LOGO_PADDING * 2
+logo_canvas = Canvas(width=logo_canvas_width, height=logo_canvas_height, highlightthickness=0)
+
+logo_canvas_img_x = logo_img_width / 2 + LOGO_PADDING
+logo_canvas_img_y = logo_img_width / 2 + LOGO_PADDING
+logo_canvas.create_image(logo_canvas_img_x, logo_canvas_img_y, image=logo_img)
+
+logo_canvas.grid(column=1, row=0)
+
+# Create the labels
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+username_label = Label(text="E-mail / Username:")
+username_label.grid(column=0, row=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+# Create the inputs
+website_input = Entry(width=40)
+website_input.grid(column=1, row=1, columnspan=2)
+website_input.focus()
+
+username_input = Entry(width=40)
+username_input.grid(column=1, row=2, columnspan=2)
+
+password_input = Entry(width=22)
+password_input.grid(column=1, row=3)
+
+# Create the buttons
+generate_pw_button = Button(text="Generate Password", command=generate_password)
+generate_pw_button.grid(column=2, row=3)
+
+add_pw_button = Button(text="Add", width=37, command=save_password)
+add_pw_button.grid(column=1, row=4, columnspan=2)
 
 # Keep window open
 window.mainloop()
