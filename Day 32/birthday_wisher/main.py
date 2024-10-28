901d24178ba6aac2a71257f39c7f36d9f63f7515
Kazoob <>
2024-10-28 18:41:13 -0500

    Day 32 birthday wisher - solution

--- Day 32/birthday_wisher/main.py
+++ Day 32/birthday_wisher/main.py
@@ -1,13 +1,58 @@
-##################### Extra Hard Starting Project ######################
+import smtplib
+from datetime import datetime
+import random
+import pandas
+import json
 
-# 1. Update the birthdays.csv
+# Birthday CSV file
+BIRTHDAY_FILE_NAME = "birthdays_pii.csv"
 
-# 2. Check if today matches a birthday in the birthdays.csv
+# List of available letter file names to choose from
+LETTER_FILE_NAMES = [
+    "letter_templates/letter_1.txt",
+    "letter_templates/letter_2.txt",
+    "letter_templates/letter_3.txt",
+]
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+# SMTP configuration file name
+SMTP_PII_FILE_NAME = "../../smtp_pii.json"
 
-# 4. Send the letter generated in step 3 to that person's email address.
+# Get the current month and day
+month = datetime.now().month
+day = datetime.now().day
 
+# Read the birthday CSV file
+birthday_data = pandas.read_csv(BIRTHDAY_FILE_NAME)
 
+# Search for birthdays matching the current month and day
+birthdays = birthday_data[(birthday_data['month'] == month) & (birthday_data['day'] == day)]
 
+# Loop through any birthdays found
+for index, row in birthdays.iterrows():
+    # Get the name and e-mail
+    name = row["name"]
+    email = row["email"]
 
+    # Choose and open a random letter, replacing [NAME] with the actual name
+    with open(random.choice(LETTER_FILE_NAMES)) as letter_file:
+        letter = letter_file.read().replace("[NAME]", name)
+
+    # Open the SMTP configuration file
+    with open(SMTP_PII_FILE_NAME) as smtp_pii_file:
+        smtp_pii = json.load(smtp_pii_file)
+
+    # Get the SMTP configuration fields
+    smtp_username = smtp_pii['username']
+    smtp_password = smtp_pii['password']
+    smtp_from = smtp_pii['from']
+    smtp_host = smtp_pii['host']
+    smtp_port = smtp_pii['port']
+
+    # Prepare the e-mail message
+    message = f"From: {smtp_from}\nTo: {email}\nSubject: Happy Birthday!\n\n{letter}"
+
+    # Send the e-mail
+    with smtplib.SMTP(host=smtp_host, port=smtp_port) as smtp:
+        smtp.starttls()
+        smtp.login(user=smtp_username, password=smtp_password)
+        smtp.sendmail(from_addr=smtp_from, to_addrs=email, msg=message)
