c673b25ed4eff2fca6883d1fa0a224b01220ba3e
Kazoob <>
2024-10-28 20:13:01 -0500

    Day 33 iss overhead - solution

--- Day 33/iss-overhead/main.py
+++ Day 33/iss-overhead/main.py
@@ -1,37 +1,80 @@
 import requests
 from datetime import datetime
+import smtplib
+import json
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = -50.1357  # Your latitude
+MY_LONG = -151.7149  # Your longitude
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
-
-#Your position is within +5 or -5 degrees of the ISS position.
+SMTP_PII_FILE_NAME = "../../smtp_pii.json"
 
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
+def is_iss_overhead(lat, long):
+    iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    iss_response.raise_for_status()
+    iss_data = iss_response.json()
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    iss_latitude = float(iss_data["iss_position"]["latitude"])
+    iss_longitude = float(iss_data["iss_position"]["longitude"])
 
-time_now = datetime.now()
+    if iss_latitude - 5 <= lat <= iss_latitude + 5 and iss_longitude - 5 <= long <= iss_longitude + 5:
+        return True
+    else:
+        return False
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
+
+def is_dark_outside(lat, long):
+    parameters = {
+        "lat": lat,
+        "lng": long,
+        "formatted": 0,
+    }
+
+    sunset_response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    sunset_response.raise_for_status()
+    sunset_data = sunset_response.json()
+
+    sunrise = int(sunset_data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(sunset_data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    hour_now = datetime.now().hour
+
+    if sunset <= hour_now <= 23:
+        return True
+    elif 0 <= hour_now <= sunrise:
+        return True
+    else:
+        return False
+
+
+iss_overhead = is_iss_overhead(MY_LAT, MY_LONG)
+dark_outside = is_dark_outside(MY_LAT, MY_LONG)
+
+if iss_overhead and dark_outside:
+    print("The ISS is overhead and it is dark outside!")
+
+    # Open the SMTP configuration file
+    with open(SMTP_PII_FILE_NAME) as smtp_pii_file:
+        smtp_pii = json.load(smtp_pii_file)
+
+    # Get the SMTP configuration fields
+    smtp_username = smtp_pii['username']
+    smtp_password = smtp_pii['password']
+    smtp_from = smtp_pii['from']
+    smtp_host = smtp_pii['host']
+    smtp_port = smtp_pii['port']
+
+    # Prepare the e-mail message
+    message = f"From: {smtp_from}\nTo: {smtp_from}\nSubject: Look up!\n\nThe ISS is overhead and it is dark outside!"
+
+    # Send the e-mail
+    with smtplib.SMTP(host=smtp_host, port=smtp_port) as smtp:
+        smtp.starttls()
+        smtp.login(user=smtp_username, password=smtp_password)
+        smtp.sendmail(from_addr=smtp_from, to_addrs=smtp_from, msg=message)
+elif iss_overhead:
+    print("The ISS is overhead but it is not dark outside.")
+else:
+    print("The ISS is not overhead.")
+
 # BONUS: run the code every 60 seconds.
-
-
-
