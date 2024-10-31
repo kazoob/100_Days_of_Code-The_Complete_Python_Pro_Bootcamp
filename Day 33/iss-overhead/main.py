f9b82906a3490a9f72024d2ae258d151f6d36013
Kazoob <>
2024-10-30 20:15:37 -0500

    Day 33 iis overhead - convert secrets to environment variables

--- Day 33/iss-overhead/main.py
+++ Day 33/iss-overhead/main.py
@@ -1,8 +1,8 @@
 import requests
 from datetime import datetime
 import smtplib
-import json
 import tzlocal
+import os
 
 MY_LAT = -50.1357  # Your latitude
 MY_LONG = -151.7149  # Your longitude
@@ -55,25 +55,14 @@ dark_outside = is_dark_outside(MY_LAT, MY_LONG)
 if iss_overhead and dark_outside:
     print("The ISS is overhead and it is dark outside!")
 
-    # Open the SMTP configuration file
-    with open(SMTP_PII_FILE_NAME) as smtp_pii_file:
-        smtp_pii = json.load(smtp_pii_file)
-
-    # Get the SMTP configuration fields
-    smtp_username = smtp_pii['username']
-    smtp_password = smtp_pii['password']
-    smtp_from = smtp_pii['from']
-    smtp_host = smtp_pii['host']
-    smtp_port = smtp_pii['port']
-
     # Prepare the e-mail message
-    message = f"From: {smtp_from}\nTo: {smtp_from}\nSubject: Look up!\n\nThe ISS is overhead and it is dark outside!"
+    message = f"From: {os.environ["SMTP_FROM"]}\nTo: {os.environ["SMTP_FROM"]}\nSubject: Look up!\n\nThe ISS is overhead and it is dark outside!"
 
     # Send the e-mail
-    with smtplib.SMTP(host=smtp_host, port=smtp_port) as smtp:
+    with smtplib.SMTP(host=os.environ["SMTP_HOST"], port=int(os.environ["SMTP_PORT"])) as smtp:
         smtp.starttls()
-        smtp.login(user=smtp_username, password=smtp_password)
-        smtp.sendmail(from_addr=smtp_from, to_addrs=smtp_from, msg=message)
+        smtp.login(user=os.environ["SMTP_USERNAME"], password=os.environ["SMTP_PASSWORD"])
+        smtp.sendmail(from_addr=os.environ["SMTP_FROM"], to_addrs=os.environ["SMTP_FROM"], msg=message)
 elif iss_overhead:
     print("The ISS is overhead but it is not dark outside.")
 else:
