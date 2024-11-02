e0ca9bd4ef15f666030abde2952321435a493d13
Kazoob <>
2024-11-02 14:21:29 -0500

    Day 39 flight tracker - data_manager.py convert to _private variable names

--- Days 30-39/Day 39/data_manager.py
+++ Days 30-39/Day 39/data_manager.py
@@ -1,35 +1,38 @@
 import os
 import requests
 
-CACHED = True
-API_ENDPOINT = (f"https://api.sheety.co/{os.environ["SHEETY_USERNAME"]}/"
-                f"{os.environ["SHEETY_FLIGHT_PROJECT_NAME"]}/"
-                f"{os.environ["SHEETY_FLIGHT_SHEET_NAME"]}")
-API_KEY = os.environ["SHEETY_API_KEY"]
-
 
 # This class is responsible for talking to the Google Sheet.
 class DataManager:
 
     def __init__(self):
+        # Determine whether to use cached API data or get live data from API.
+        self._use_cached_json = os.environ.get("USE_CACHED_JSON", "False")
+
+        # API information.
+        self._api_endpoint = (f"https://api.sheety.co/{os.environ["SHEETY_USERNAME"]}/"
+                              f"{os.environ["SHEETY_FLIGHT_PROJECT_NAME"]}/"
+                              f"{os.environ["SHEETY_FLIGHT_SHEET_NAME"]}")
+        self._api_key = os.environ["SHEETY_API_KEY"]
+
         # API authentication headers.
-        self.api_headers = {
-            "Authorization": f"Bearer {API_KEY}",
+        self._api_headers = {
+            "Authorization": f"Bearer {self._api_key}",
         }
 
         # Get live results from API.
-        if not CACHED:
+        if self._use_cached_json != "True":
             # API request.
-            response = requests.get(url=API_ENDPOINT, headers=self.api_headers)
+            response = requests.get(url=self._api_endpoint, headers=self._api_headers)
             response.raise_for_status()
 
             # Get JSON data.
             self.sheet_data = response.json()["prices"]
         # Use cached API data.
         else:
-            self.sheet_data = [{'city': 'Tokyo', 'iataCode': 'TEST', 'lowestPrice': 485, 'id': 2},
-                               {'city': 'San Francisco', 'iataCode': 'TEST', 'lowestPrice': 260, 'id': 3},
-                               {'city': 'Dublin', 'iataCode': 'TEST', 'lowestPrice': 378, 'id': 4}]
+            self.sheet_data = [{'city': 'Tokyo', 'iataCode': '', 'lowestPrice': 485, 'id': 2},
+                               {'city': 'San Francisco', 'iataCode': '', 'lowestPrice': 260, 'id': 3},
+                               {'city': 'Dublin', 'iataCode': '', 'lowestPrice': 378, 'id': 4}]
 
     def get_sheet_data(self):
         """Return flight sheet data."""
@@ -53,7 +56,8 @@ class DataManager:
                     "iataCode": row["iataCode"],
                 }
             }
-            response = requests.put(url=f"{API_ENDPOINT}/{row["id"]}", headers=self.api_headers, json=update_params)
+            response = requests.put(url=f"{self._api_endpoint}/{row["id"]}", headers=self._api_headers,
+                                    json=update_params)
             response.raise_for_status()
         # No row was found.
         else:
