e01f146e25082d3148703b394c2066e5d790c67b
Kazoob <>
2024-11-02 16:35:09 -0500

    Day 39 flight tracker - flight_search.py get access token

--- Days 30-39/Day 39/flight_search.py
+++ Days 30-39/Day 39/flight_search.py
@@ -1,16 +1,41 @@
 import os
 import requests
 
-API_ENDPOINT = "https://test.api.amadeus.com"
-API_KEY = os.environ["AMADEUS_TEST_API_KEY"]
-API_SECRET = os.environ["AMADEUS_TEST_API_SECRET"]
-
 
 # This class is responsible for talking to the Flight Search API.
 class FlightSearch:
 
     def __init__(self):
-        pass
+        # API information.
+        self._api_endpoint = "https://test.api.amadeus.com/v1"
+        self._api_key = os.environ["AMADEUS_TEST_API_KEY"]
+        self._api_secret = os.environ["AMADEUS_TEST_API_SECRET"]
+
+        # Get API bearer token.
+        self._token = self._get_new_token()
+
+    def _get_new_token(self):
+        # Token API information.
+        token_api_endpoint = "https://test.api.amadeus.com/v1/security/oauth2/token"
+
+        # Token API headers.
+        token_api_header = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        # Token API body.
+        token_api_body = {
+            'grant_type': 'client_credentials',
+            'client_id': self._api_key,
+            'client_secret': self._api_secret
+        }
+
+        # API request.
+        response = requests.post(url=token_api_endpoint, headers=token_api_header, data=token_api_body)
+        response.raise_for_status()
+
+        # Return access token.
+        return response.json()["access_token"]
 
     def get_iata_code(self, city):
         return "TEST"
