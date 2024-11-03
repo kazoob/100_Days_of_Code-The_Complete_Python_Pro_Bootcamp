efa46484f63a06ee1d0982c90ba4ee41a7ea2d50
Kazoob <>
2024-11-02 19:06:36 -0500

    Day 39 flight tracker - flight_search.py iata code search

--- Days 30-39/Day 39/flight_search.py
+++ Days 30-39/Day 39/flight_search.py
@@ -7,16 +7,19 @@ class FlightSearch:
 
     def __init__(self):
         # API information.
-        self._api_endpoint = "https://test.api.amadeus.com/v1"
+        self._api_endpoint_base = "https://test.api.amadeus.com/v1"
         self._api_key = os.environ["AMADEUS_TEST_API_KEY"]
         self._api_secret = os.environ["AMADEUS_TEST_API_SECRET"]
 
         # Get API access token.
-        self._token = self._get_new_token()
+        self._api_token = self._get_new_token()
+        self._api_header = {
+            "Authorization": f"Bearer {self._api_token}",
+        }
 
     def _get_new_token(self):
         # Token API information.
-        token_api_endpoint = "https://test.api.amadeus.com/v1/security/oauth2/token"
+        token_api_endpoint = f"{self._api_endpoint_base}/security/oauth2/token"
 
         # Token API headers.
         token_api_header = {
@@ -38,4 +41,19 @@ class FlightSearch:
         return response.json()["access_token"]
 
     def get_iata_code(self, city):
+        api_endpoint = f"{self._api_endpoint_base}/reference-data/locations/cities"
+
+        api_parameters = {
+            "keyword": city,
+            "countryCode": "US"
+        }
+
+        response = requests.get(url=api_endpoint, headers=self._api_header, params=api_parameters)
+        response.raise_for_status()
+
+        print(response.json())
+
+        # Return access token.
+        # return response.json()["access_token"]
+
         return "TEST"
