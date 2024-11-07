b4546c203a6e30adb3ed39f819be2b0c304f40ad
Kazoob <>
2024-11-07 00:01:47 -0600

    Day 45 ycombinator scraper - solution

---
+++ Days 40-49/Day 45/main.py
@@ -0,0 +1,31 @@
+from operator import indexOf
+
+from bs4 import BeautifulSoup
+import requests
+# import lxml
+
+# Get the website
+website = requests.get(url="https://news.ycombinator.com/news")
+soup = BeautifulSoup(markup=website.text, features="html.parser")
+
+# Get the article titles
+article_titles = [span.find(name="a").get_text() for span in soup.find_all(name="span", class_="titleline")]
+
+# Get the article links
+article_links = [span.find(name="a").get("href") for span in soup.find_all(name="span", class_="titleline")]
+
+# Get the article upvotes
+article_upvotes = [int(span.get_text().split(" ")[0]) for span in soup.find_all(name="span", class_="score")]
+
+print(article_titles)
+print(article_links)
+print(article_upvotes)
+
+print()
+
+max_upvotes = max(article_upvotes)
+article_num = article_upvotes.index(max_upvotes)
+
+print(article_titles[article_num])
+print(article_links[article_num])
+print(article_upvotes[article_num])
