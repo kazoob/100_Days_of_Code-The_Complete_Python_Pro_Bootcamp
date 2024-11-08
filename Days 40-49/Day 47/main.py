8ce80d10ec714203fd770ae39c9059cf56a0c1c5
Kazoob <>
2024-11-07 20:13:21 -0600

    Day 47 Amazon price tracker - add request headers

--- Days 40-49/Day 47/main.py
+++ Days 40-49/Day 47/main.py
@@ -9,7 +9,23 @@ PRACTICE_URL = "https://appbrewery.github.io/instant_pot/"
 LIVE_URL = "https://www.amazon.com/dp/B075CYMYK6"
 
 # Get webpage.
-response = requests.get(url=PRACTICE_URL)
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-US,en;q=0.9,en-CA;q=0.8",
+    "Priority": "u=0, i",
+    "Sec-Ch-Ua": "\"Chromium\";v=\"130\", \"Microsoft Edge\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": "\"macOS\"",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0",
+}
+
+response = requests.get(url=PRACTICE_URL, headers=headers)
 soup = BeautifulSoup(markup=response.text, features="html.parser")
 
 # Get the price.
@@ -20,7 +36,7 @@ print(f"${price}")
 # Check the price against the threshold.
 if price <= PRICE_THRESHOLD:
     # Get the title
-    title = soup.find(id="productTitle").get_text().split("\r\n")[0].strip()
+    title = soup.find(id="productTitle").get_text().split("\r\n")[0].split(",")[0].strip()
 
     print(title)
 
